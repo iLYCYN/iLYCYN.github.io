f878d5f6a3e3305341e52f5cef918d8b786e0265
iLYCYN <86775114+iLYCYN@users.noreply.github.com>
2021-07-02 13:46:49 +0800

    Create thumbnail.js

---
+++ thumbnail.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let targetImage = document.querySelector("#smart-image");
+	targetImage.addEventListener("click", function() {
+	    alert(targetImage.classList.remove("small"))
+	});
+	
+});
