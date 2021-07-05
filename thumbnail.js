c6ed3a9065df98cf4278d5ed6786ba65c99828ff
iLYCYN <86775114+iLYCYN@users.noreply.github.com>
2021-07-05 22:25:32 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,10 +1,68 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	
-    let targetImage = document.querySelector("#smart-image");
-	
-	targetImage.addEventListener("click", function() {
-	
-	    targetImage.classList.remove("small")
+	// 第一张图片
+    let targetImage1 = document.querySelector("#smart-image1");
+	targetImage1.addEventListener("click", function() {
 		
+		if (targetImage1.classList.contains("small")) {
+		    targetImage1.classList.remove("small");
+		} else {
+		    targetImage1.classList.add("small");
+		} 
 	});
+	
+	// 第二张图片
+	let targetImage2 = document.querySelector("#smart-image2");
+	targetImage2.addEventListener("click", function() {
+		
+		if (targetImage2.classList.contains("small")) {
+		    targetImage2.classList.remove("small");
+		} else {
+		    targetImage2.classList.add("small");
+		} 
+	});
+	
+	// 第三张图片
+	let targetImage3 = document.querySelector("#smart-image3");
+	targetImage3.addEventListener("click", function() {
+		
+		if (targetImage3.classList.contains("small")) {
+		    targetImage3.classList.remove("small");
+		} else {
+		    targetImage3.classList.add("small");
+		} 
+	});
+	
+	// 第四张图片
+	let targetImage4 = document.querySelector("#smart-image4");
+	targetImage4.addEventListener("click", function() {
+		
+		if (targetImage4.classList.contains("small")) {
+		    targetImage4.classList.remove("small");
+		} else {
+		    targetImage4.classList.add("small");
+		} 
+	});
+	
+	// 第五张图片
+	let targetImage5 = document.querySelector("#smart-image5");
+	targetImage5.addEventListener("click", function() {
+		
+		if (targetImage5.classList.contains("small")) {
+		    targetImage5.classList.remove("small");
+		} else {
+		    targetImage5.classList.add("small");
+		} 
+	});
+	
+	// 第六张图片
+	let targetImage6 = document.querySelector("#smart-image6");
+	targetImage6.addEventListener("click", function() {
+		
+		if (targetImage6.classList.contains("small")) {
+		    targetImage6.classList.remove("small");
+		} else {
+		    targetImage6.classList.add("small");
+		} 
+	});
+
 });
